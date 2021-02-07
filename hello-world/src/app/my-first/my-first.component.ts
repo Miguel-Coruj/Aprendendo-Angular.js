dddd8ddea82bf58d097be2208ea34729e67d16f1
Miguel-Coruj <miguelcoruj@gmail.com>
2021-02-07 18:52:48 -0300

    my first component

---
+++ hello-world/src/app/my-first/my-first.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-first-component',
+    template: '<p>My first Component in angular</p>'
+})
+
+export class MyFirstComponent { }
