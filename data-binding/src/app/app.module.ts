0e6c6a211495bc43379836bca50ea8caabd867b5
Miguel-Coruj <miguelcoruj@gmail.com>
2021-02-09 15:18:45 -0300

    property & two way | binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,10 +9,11 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 @NgModule({
   declarations: [	
     AppComponent,
-      DataBindingComponent
+    DataBindingComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = 'https://miguel-coruj.github.io'
+  urlImg: string = 'http://lorempixel.com/400/200/nature/'
+
+  valueInput: string = ''
+
+  isMouseOver: boolean = false
+
+  name: string = 'abc'
+  
+  buttonClicked() {
+    alert(`Você disse ${this.valueInput}`)
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.valueInput = (<HTMLInputElement>event.target).value
+  }
+
+  OnMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
+  }
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
