62b7d03f3631159f33c5c8208c57239b1c88930f
Miguel-Coruj <miguelcoruj@gmail.com>
2021-02-15 09:54:13 -0300

    📦 NEW: component

---
+++ directives/src/app/ngif/ngif.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngif',
+  templateUrl: './ngif.component.html',
+  styleUrls: ['./ngif.component.css']
+})
+export class NgifComponent implements OnInit {
+
+  users: string[] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
